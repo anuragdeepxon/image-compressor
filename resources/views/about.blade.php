68a22972e3c00ee0705036974d23e6583b4b553a
AnuragDeep <anuragdeep.xon@gmail.com>
2022-04-29 12:03:35 +0530

    UI Updates

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -26,6 +26,8 @@
 </head>
 
 <body class="min-h-screen">
+
+
     <!-- Header start  -->
     <div id="arya-fly-wrap">
         <div id="arya-fly-menu-top" class="left relative pt-3">
@@ -33,7 +35,7 @@
                 <div class="arya-fly-top-in">
                     <div id="arya-fly-logo" class="left relative logo">
                         <!-- <a href="/"><img src="{{ URL::asset('images/logo.png') }}" class="" alt="Image To 50kb" /> -->
-                        <h1 class="text-2xl font-bold ">Image To 50kb</h1>
+                        <h1 class="text-2xl font-bold text-white">Image To 50kb</h1>
                         </a>
                     </div>
                 </div>
@@ -64,7 +66,7 @@
     </div>
 
     <header>
-        <nav class="fixed mx-auto top-0 left-0 right-0 z-[500] shadow-xl flex bg-[#2BC380] 2xl:px-[11%] xl:px-[11%] lg:px-[11%] md:px-[10rem] sm:px-10 px-5 ">
+        <nav class="fixed mx-auto top-0 left-0 right-0 z-[500] shadow-xl flex bg-[#0f5dfa] 2xl:px-[11%] xl:px-[11%] lg:px-[11%] md:px-[10rem] sm:px-10 px-5 ">
 
             <!-- Left Navigation -->
             <div class="flex-1 flex justify-center mr-auto">
@@ -87,7 +89,7 @@
                 <div class="">
 
                     <!-- <a href="/"><img src="{{ URL::asset('images/logo.png') }}" class="w-16 md:w-20 lg:w-20 p-2 mt-5 inline" alt="Image To 50kb" /> -->
-                    <h1 class="mt-5 p-2 font-bold text-white text-xl md:text-3xl lg:text-3xl  self-center" style="float: right;">Image To 50kb</h1>
+                    <h1 class="mt-3 p-2 font-bold text-white text-xl md:text-3xl lg:text-3xl  self-center" style="float: right;">Image To 50kb</h1>
                     </a>
 
                 </div>
@@ -109,6 +111,7 @@
 
     <div id="overlay" class="arya-fly-fade"></div>
     <!-- Header end  -->
+
 
 
 
@@ -137,13 +140,17 @@
             <svg width="50" height="50" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" class="inline-block fill-white">
                 <path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path>
             </svg>
-            <p class="text-white">Copyright &copy;<script>
+            <p class="text-white mt-10">Copyright &copy;<script>
                     var CurrentYear = new Date().getFullYear()
                     document.write(CurrentYear)
-                </script> Designed &amp; Developed by <a class="text-white" href="https://anuragdeep.com">Anurag Deep</a></p>
+                </script> <span class="font-bold">ImageTo50kb</span>
+                <br>
+                <br>
+                Developed by <a class="text-white font-bold hover:text-green-400" href="https://anuragdeep.com">Anurag Deep</a>
+            </p>
         </div>
         <div>
-            <div class="grid grid-flow-col gap-4">
+            <!-- <div class="grid grid-flow-col gap-4">
                 <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-white">
                         <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
                     </svg></a>
@@ -153,7 +160,7 @@
                 <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-white">
                         <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
                     </svg></a>
-            </div>
+            </div> -->
         </div>
     </footer>
     <!-- /footer -->
